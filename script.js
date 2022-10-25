783aaa536e12f0df37542c63ec2d7c730398fedb
Riley Rahe <rilrahe@gmail.com>
2022-10-24 19:46:32 -0500

    more api

--- script.js
+++ script.js
@@ -1,11 +1,50 @@
 var userQuery = $("#searchbox").val();
 
-var tasteDiveURL = 'https://tastedive.com/api/similar?q=';
-var omdbUrl='http://www.omdbapi.com/?apikey=f217c7ff'
+var tasteDiveURL = "https://tastedive.com/api/similar?q=";
+var omdbUrl = "http://www.omdbapi.com/?apikey=f217c7ff";
 
-  tasteDiveURL = tasteDiveURL + userQuery +'type=movies'+ 'k=443649-RileyRah-2GRRJ49E';
+tasteDiveURL =
+  tasteDiveURL + userQuery + "type=movies" + "k=443649-RileyRah-2GRRJ49E";
 
-tasteDiveURL = tasteDiveURL + '&q=' + query;
-omdbUrl = omdbUrl + 't=' + userQuery; 
+tasteDiveURL = tasteDiveURL + "&q=" + query;
+omdbUrl = omdbUrl + "t=" + userQuery;
 
-var combined = ''
+var combined = tasteDiveURL + omdbUrl;
+
+fetch(combined, { mode: "cors" }).then(function (response) {
+  // Check the console first to see the response.status
+  console.log(combined.status);
+
+  // get data
+  combined.json().then(function (data) {
+    displayData(data);
+  });
+});
+
+function displayData(data) {
+  console.log(data.results);
+  for (i = 0; i < data.results.length; i++) {
+    console.log(data.results[i].title);
+  }
+
+  var tvScreen = $("#tvScreen");
+  tvScreen.append(
+    "<tr>" +
+      "<td>" +
+      (i + 1) +
+      "</td>" +
+      "<td>" +
+      data.results[i].title +
+      "</td>" +
+      "<td>" +
+      data.results[i].date +
+      "</td>" +
+      "<td>" +
+      subjects +
+      "</td>" +
+      "<td><a href=" +
+      data.results[i].id +
+      ">Read more</a></td>" +
+      +"</tr>"
+  );
+}
