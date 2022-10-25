5875468122d1dc2b6f7d500e447c7095677473a3
Riley Rahe <rilrahe@gmail.com>
2022-10-24 19:24:28 -0500

    working on api

--- script.js
+++ script.js
@@ -1,37 +1,11 @@
-var resultTextEl = document.querySelector('#searchbox');
+var userQuery = $("#searchbox").val();
 
 var tasteDiveURL = 'https://tastedive.com/api/similar?q=';
+var omdbUrl='http://www.omdbapi.com/?apikey=f217c7ff'
 
-if (format) {
-  tasteDiveURL = 'https://tastedive.com/api/similar' + format +'type=movies'+ 'k=443649-RileyRah-2GRRJ49E';
-}
+  tasteDiveURL = tasteDiveURL + userQuery +'type=movies'+ 'k=443649-RileyRah-2GRRJ49E';
 
 tasteDiveURL = tasteDiveURL + '&q=' + query;
+omdbUrl = omdbUrl + 't=' + userQuery; 
 
-fetch(tasteDiveURL)
-  .then(function (response) {
-    if (!response.ok) {
-      throw response.json();
-    }
-
-    return response.json();
-  })
-  .then(function (locRes) {
-    // write query to page so user knows what they are viewing
-    resultTextEl.textContent = locRes.search.query;
-
-    console.log(locRes);
-
-    if (!locRes.results.length) {
-      console.log('No results found!');
-      resultContentEl.innerHTML = '<h3>No results found, search again!</h3>';
-    } else {
-      resultContentEl.textContent = '';
-      for (var i = 0; i < locRes.results.length; i++) {
-        printResults(locRes.results[i]);
-      }
-    }
-  })
-  .catch(function (error) {
-    console.error(error);
-  });
+var combined = ''
