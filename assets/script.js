d9759e3df74d14449bca0b936d4a592b48a495b6
karasu yami <demigodlinty@gmail.com>
2022-10-27 21:49:55 -0500

    got api to work and show console log info

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,20 @@
-var resultTextEl = document.querySelector('#searchbox');
+var resultTextEl = document.querySelector('#Searchbar');
 var searchButton = document.getElementById('searchMovie')
-var tasteDiveURL = 'https://api.watchmode.com/v1/autocomplete-search/?apiKey=9RuErWa5jOWO6YQFVTeRQJf8oFkOhacTsx2S5D1z&search_value=The%20Avengers&search_type=3';
+var tasteDiveURL = 'https://api.watchmode.com/v1/autocomplete-search/?apiKey=9RuErWa5jOWO6YQFVTeRQJf8oFkOhacTsx2S5D1z&search_type=3';
  function fetchMovie(){
+var userInput = resultTextEl.value
 
-fetch(tasteDiveURL)
+var url = tasteDiveURL + "&search_value=" + userInput
+fetch(url)
   .then(response => response.json())
-  .then(data => console.log(data))
+  .then(data => {
+    console.log(data.results)
+  
+  }
+  
+  )
+
+
 }
 
+$('#searchMovie').on('click',fetchMovie)
