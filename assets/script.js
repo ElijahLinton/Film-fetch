66edad54003dadb826ed2460a1f9565f0f41284c
karasu yami <demigodlinty@gmail.com>
2022-10-30 04:01:39 -0500

    add overflow scroll and once:true statement to prevent errors

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,9 @@
 var resultTextEl = document.querySelector('#Searchbar');
 var searchButton = document.getElementById('searchMovie')
 var tasteDiveURL = 'https://api.watchmode.com/v1/autocomplete-search/?apiKey=9RuErWa5jOWO6YQFVTeRQJf8oFkOhacTsx2S5D1z&search_type=3';
- function fetchMovie(){
+
+
+searchButton.addEventListener('click',function fetchMovie(){
 var userInput = resultTextEl.value
 
 var url = tasteDiveURL + "&search_value=" + userInput
@@ -11,20 +13,21 @@ fetch(url)
   const list = data.results
   list.map((item)=>{
     const name = item.name;
-    const movie = `<tr> <h2>${name}</h2> </tr>`
+    const poster = item.image_url
+    const movie = `<tr> <img src="${poster}"> <h2>${name}</h2> </tr>`
     document.querySelector('.turnedOn').innerHTML += movie
   }) 
 })
+if(userInput === ""){
+  alert("invalid: please make sure to type a movie.")
+
+} 
 if(fetchMovie){
   var Television = document.querySelector('.tvScreen')
   Television.classList.remove("tvScreen")
   Television.classList.add("turnedOn")
 }
 
- }
 
+ },{once:true})
 
-
-
-
-$('#searchMovie').on('click',fetchMovie)
