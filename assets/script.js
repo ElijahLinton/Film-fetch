5e08fe2ea8305366688e2ca473be58f9e9cb1a9e
Riley Rahe <rilrahe@gmail.com>
2022-10-31 19:34:40 -0500

    clock for real this time

--- assets/script.js
+++ assets/script.js
@@ -35,3 +35,9 @@ if(fetchMovie){
 
  },{once:true})
 
+ var timeDisplayEl = $("#time-display");
+ function displayTime() {
+   var rightNow = moment().format("MMM DD, YYYY [at] hh:mm:ss a");
+   timeDisplayEl.text(rightNow);
+ }
+ setInterval(displayTime, 1000);
